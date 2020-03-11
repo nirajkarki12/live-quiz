edf0d39b3d978847d46256d407aecf9115088e93
Niraj Karki <nirajkarki12@gmail.com>
2020-03-11 14:49:24 +0545

    changes

--- backend_bak/src/sockets/interfaces/message.interface.ts
+++
@@ -1,9 +0,0 @@
-import * as Document from 'mongoose';
-
-export interface Message extends Document {
-   message: string;
-   userName: string;
-   userImage: string;
-   isAdminUser: Boolean;
-   createdAt: Date;
-}
--- backend_bak/src/users/ro/user.ro.ts
+++
@@ -1,5 +0,0 @@
-export interface UserRO {
-   name: string;
-   email: string;
-   image: string;
-}
